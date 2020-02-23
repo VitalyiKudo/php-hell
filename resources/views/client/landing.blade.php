94b845a23df8f514e3beb0c6de71286b90fcbc43
Muhammad Nadeem <nd.maqbool@gmail.com>
2020-02-23 18:54:28 +0200

    main page changes done

--- resources/views/client/landing.blade.php
+++ resources/views/client/landing.blade.php
@@ -151,9 +151,21 @@
     <div class="container services services-right mt-0">
         <div class="row">
             <div class="col-md-6 services-text">
-                <h2 class="services-h2 color-white">Luxury Travel | Luxury Vacations</h2>
-                <p class="services-p color-white">You can’t experience luxury travel or luxury vacations by starting them off in economy class on a commercial airliner. With JetOnset, your luxurious getaways will get off on the right foot with a world-class experience on a private chartered jet. We have access to almost any jet size, shape, and class you can imagine. It doesn’t matter if you want to travel to a nearby island or halfway around the world - we have the jets for you! World-class!</p>
+
+                <h5 class="services-h2 color-white">Luxury Travel, Luxury Vacations</h5>
+                <p class="services-p color-white">You can’t experience luxury travel or luxury vacations by starting them off in economy class on a commercial airliner. With JetOnset, your luxurious getaways will get off on the right foot with a world-class experience on a private chartered jet. </p>
+                <div class="row mt-5">
+                    <div class="col-md-1 pr-md-0">
+                        <img src="/images/line.png" alt="line">
+                    </div>
+                    <div class="col-md-10 service-fancy-p">
+                        <p>We have access to almost any jet size, shape, and class you can imagine. It doesn’t matter if you want to travel to a nearby island or halfway around the world - we have the jets for you! World-class!</p>
+                    </div>
+                </div>
             </div>
+
+
+
             <div class="col-md-6 services-image luxury-travel-image"></div>
         </div>
     </div>
@@ -161,8 +173,17 @@
         <div class="row">
             <div class="col-md-6 services-image corporate-travel-image"></div>
             <div class="col-md-6 services-text">
-                <h2 class="services-h2 color-white">Corporate Travel | Corporate Retreats</h2>
-                <p class="services-p color-white">Treat your executives or clients like the valued stakeholders that they are with the use of private jets and chartered planes for your next shareholder meeting. Have a once-in-a-lifetime client potential or want to treat your executives to a special treat? With JetOnset you can rest assured that nothing will surpass this revolutionary way of travel. Spontaneous new sales meetings or pre-arranged travel plans are no problem, and when your clients know how much you care about their experience, they will return the same treatment.</p>
+                <h2 class="services-h2 color-white">Corporate Travel, Corporate Retreats</h2>
+                <p class="services-p color-white">Treat your executives or clients like the valued stakeholders that they are with the use of private jets and chartered planes for your next shareholder meeting. Have a once-in-a-lifetime client potential or want to treat your executives to a special treat? </p>
+
+                <div class="row mt-5">
+                    <div class="col-md-1 pr-md-0">
+                        <img src="/images/line2.png" alt="line">
+                    </div>
+                    <div class="col-md-11 service-fancy-p">
+                        <p>With JetOnset you can rest assured that nothing will surpass this revolutionary way of travel. Spontaneous new sales meetings or pre-arranged travel plans are no problem, and when your clients know how much you care about their experience, they will return the same treatment.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -205,16 +226,23 @@
                 <h2 class="mb-3">About Us</h2>
             </div>
             <div class="col-md-6">
-                <p>We are a group of people who wanted to apply the social and technological revolution found in cars yesterday, to the private jets of today and tomorrow.</p>
-                <p>We have started on our mission: To use crowdsourcing technology to revolutionize the way we think of private jets. We realize this is only the beginning, and we are helping to push the private jet industry into the hands of the few into the hands of many.</p>
+                <p><strong>We are a group of people</strong> who wanted to apply the social and technological revolution found in cars yesterday, to the private jets of today and tomorrow.</p>
+                <p><strong>We have started on our mission:</strong> To use crowdsourcing technology to revolutionize the way we think of private jets. We realize this is only the beginning, and we are helping to push the private jet industry into the hands of the few into the hands of many.</p>
             </div>
             <div class="col-md-6">
-                <p>We have developed a proprietary system that allows you to skip middlemen and legal kickback costs and directly source your own private jet travel, regardless of the purpose or the destination.</p>
-                <p>We have the ability in real-time to show you every plane, from the small HondaJet to the world renowned Boeing 737 that you can choose to fly to your destination in class.</p>
+                <p><strong>We have developed a proprietary system</strong> that allows you to skip middlemen and legal kickback costs and directly source your own private jet travel, regardless of the purpose or the destination.</p>
+                <p>We have the ability in <strong> real-time </strong> to show you every plane, from the small <strong> HondaJet </strong> to the world renowned <strong> Boeing 737 </strong> that you can choose to fly to your destination in class.</p>
             </div>
-            <div class="col-md-12">
-                <a href="{{ url('about') }}" class="about-us-p-text">Learn more about JetOnset Co.</a>
+
+            <div class="col-md-12 mt-5">
+
+                <div class="offset-5 col-md-4 ">
+                    <a href="{{ url('about') }}" class="learn-more">More about JetOnset Co.</a>
+                </div>
+
             </div>
+
+
         </div>
     </div>
 </div>
@@ -429,11 +457,24 @@
                     <div class="card-body">
                         <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq1" role="button" aria-expanded="false" aria-controls="collapseFaq1">What is Private Jet Charter?</a>
                         <div class="collapse" id="collapseFaq1">
-                            <p class="mt-3 mb-3">JetOnset is the first platform in the world to allow you to comfortably fly on a private jet without having to own one. You can book on the 1700 plus private jets in the shortest time possible. Select from the four categories outlined; light jets, mid-size jets, long-range (heavy jets), and short-range turboprops for your appropriate fit. We guarantee you the best fit with the lowest prices on booking, for each jet category. Once you have your best jet choice, confirm your booking by paying either by Bitcoin, Credit card, Etherium, or wired transfer. Your flight is approved, and you can go ahead and start planning for your travel.</p>
-                            <p class="mb-0"><strong>We offer you three convenient options:</strong></p>
-                            <p class="mb-0"><strong>Door to Door</strong> - You are picked up from your doorstep and dropped at the doorstep of your destination.</p>
-                            <p class="mb-0"><strong>Airport to Airport</strong> - From your departure airport to your arrival airport, everything is personally managed.</p>
-                            <p class="mb-0"><strong>Flight Auctions</strong> - If your schedule is flexible; there are discounted prices for sale available for you.</p>
+                            <div class="row col-md-12 pos-full">
+                                <div class="col-md-1 line-border">
+                                    <svg width="3" height="270" viewBox="0 0 3 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <rect opacity="0.1" width="3" height="364" fill="#4c4d4c"/>
+                                    </svg>
+
+                                </div>
+                                
+                                <div class="col-md-11 position">
+                                    <p class="mt-3 mb-3">JetOnset is the first platform in the world to allow you to comfortably fly on a private jet without having to own one. You can book on the 1700 plus private jets in the shortest time possible. Select from the four categories outlined; light jets, mid-size jets, long-range (heavy jets), and short-range turboprops for your appropriate fit. 
+                                        </p>
+                                    <p class="mt-3 mb-3">We guarantee you the best fit with the lowest prices on booking, for each jet category. Once you have your best jet choice, confirm your booking by paying either by Bitcoin, Credit card, Etherium, or wired transfer. Your flight is approved, and you can go ahead and start planning for your travel.</p>    
+                                    <p class="mb-0"><strong>We offer you three convenient options:</strong></p>
+                                    <p class="mb-0"><strong>Door to Door</strong> - You are picked up from your doorstep and dropped at the doorstep of your destination.</p>
+                                    <p class="mb-0"><strong>Airport to Airport</strong> - From your departure airport to your arrival airport, everything is personally managed.</p>
+                                    <p class="mb-0"><strong>Flight Auctions</strong> - If your schedule is flexible; there are discounted prices for sale available for you.</p>
+                                </div>
+                            </div>    
                         </div>
                     </div>
                 </div>
@@ -441,31 +482,69 @@
                     <div class="card-body">
                         <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq2" role="button" aria-expanded="false" aria-controls="collapseFaq2">How Much Does Private Charter Jet Cost?</a>
                         <div class="collapse" id="collapseFaq2">
-                            <p class="mt-3 mb-0">JetOnset looks into its over 1700 available jets on the dates you are placing your reservation, and we ensure you get the lowest price possible. If you find another jet flight for less than we offer you, we will match the price and give you a 5% discount, if it is the same jet type, and the same travel route at the same time and date as yours.</p>
+                            <div class="row col-md-12 pos-full">
+                                <div class="col-md-1 line-border mt-3">
+                                    <svg width="3" height="80" viewBox="0 0 3 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <rect opacity="0.1" width="10" height="364" fill="#4c4d4c"/>
+                                    </svg>
+                                </div>
+                                <div class="col-md-11 position">
+                                    <p class="mt-3 mb-0">JetOnset looks into its over 1700 available jets on the dates you are placing your reservation, and we ensure you get the lowest price possible. If you find another jet flight for less than we offer you, we will match the price and give you a 5% discount, if it is the same jet type, and the same travel route at the same time and date as yours.</p>
+                                </div>
+                                </div>    
                         </div>
                     </div>
                 </div>
                 <div class="card mb-3">
-                    <div class="card-body">
-                        <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq3" role="button" aria-expanded="false" aria-controls="collapseFaq3">Where To Charter A Private Jet?</a>
-                        <div class="collapse" id="collapseFaq3">
-                            <p class="mt-3 mb-0">JetOnset stays with you every step of the way and does not need any membership commitments. Also, with JetOnset, you only pay for your airtime, no more and no less. You are relaxing when the jet engine begins to when it stops.</p>
+                        <div class="card-body">
+                            <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq3" role="button" aria-expanded="false" aria-controls="collapseFaq3">Where To Charter A Private Jet?</a>
+                            <div class="collapse" id="collapseFaq3">
+                            <div class="row col-md-12 pos-full">
+                                    <div class="col-md-1 line-border mt-3">
+                                        <svg width="5" height="50" viewBox="0 0 3 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect opacity="0.1" width="10" height="364" fill="#4c4d4c"/>
+                                        </svg>
+                                    </div>
+                                    <div class="col-md-11 position">
+                                        <p class="mt-3 mb-0">JetOnset stays with you every step of the way and does not need any membership commitments. Also, with JetOnset, you only pay for your airtime, no more and no less. You are relaxing when the jet engine begins to when it stops.</p>
+                                    </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div>    
                 <div class="card mb-3">
-                    <div class="card-body">
-                        <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq4" role="button" aria-expanded="false" aria-controls="collapseFaq4">Why Charter A Private Jet?</a>
-                        <div class="collapse" id="collapseFaq4">
-                            <p class="mt-3 mb-0">There are many reasons people and organizations charter private jets. It could be a romantic getaway for a grand honeymoon. It could also be a corporate retreat to reward your executives and employees. Some companies own their own planes and use these to avoid creating rumors as they visit their manufacturing facility unannounced.</p>
+                        <div class="card-body">
+                            <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq4" role="button" aria-expanded="false" aria-controls="collapseFaq4">Why Charter A Private Jet?</a>
+                            <div class="collapse" id="collapseFaq4">
+                                <div class="row col-md-12 pos-full">
+                                    <div class="col-md-1 line-border mt-4">
+                                        <svg width="5" height="60" viewBox="0 0 3 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect opacity="0.1" width="10" height="364" fill="#4c4d4c"/>
+                                        </svg>
+                                    </div>
+                                    <div class="col-md-11 position">
+                                        <p class="mt-3 mb-0">There are many reasons people and organizations charter private jets. It could be a romantic getaway for a grand honeymoon. It could also be a corporate retreat to reward your executives and employees. Some companies own their own planes and use these to avoid creating rumors as they visit their manufacturing facility unannounced.</p>
+                            
+                                    </div>
+                            </div>        
                         </div>
                     </div>
-                </div>
+                </div>    
                 <div class="card mb-3">
                     <div class="card-body">
                         <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq5" role="button" aria-expanded="false" aria-controls="collapseFaq5">How Long To Charter A Private Jet?</a>
                         <div class="collapse" id="collapseFaq5">
-                            <p class="mt-3 mb-0">JetOnset operates around the clock, and booking two hours before departure is okay, but maybe affected by positions that may arise. Booking in advance is advised.</p>
+                            <div class="row col-md-12 pos-full">
+                                <div class="col-md-1 line-border mt-4">
+                                    <svg width="5" height="40" viewBox="0 0 3 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <rect opacity="0.1" width="10" height="364" fill="#4c4d4c"/>
+                                    </svg>
+                                </div>
+                                <div class="col-md-11 position">
+                                    <p class="mt-3 mb-0">JetOnset operates around the clock, and booking two hours before departure is okay, but maybe affected by positions that may arise. Booking in advance is advised.</p>
+                    
+                                </div>
+                            </div>    
                         </div>
                     </div>
                 </div>
@@ -473,7 +552,12 @@
                     <div class="card-body">
                         <a class="card-title font-weight-bold mb-0 collapsed" data-toggle="collapse" href="#collapseFaq6" role="button" aria-expanded="false" aria-controls="collapseFaq6">Can I Charter A Private Jet?</a>
                         <div class="collapse" id="collapseFaq6">
-                            <p class="mt-3 mb-0">As long as you aren’t on a No-Fly List and have the funds available to pay for the service, you absolutely can!</p>
+                            <div class="row">
+                                
+                                <div class="col-md-12 ">
+                                    <p class="mt-3 mb-0">As long as you aren’t on a No-Fly List and have the funds available to pay for the service, you absolutely can!</p>
+                                </div>
+                            </div>        
                         </div>
                     </div>
                 </div>
@@ -511,7 +595,7 @@
         $(function() {
 
             $('.carousel').carousel({
-                interval: 3000
+                interval: 25000
             });
 
             $('input[name="departure"]').daterangepicker({
