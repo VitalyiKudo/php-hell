ea1be64123fbbe5c21c8c9471f31436729e8b5db
Muhammad Nadeem <nd.maqbool@gmail.com>
2020-08-11 19:53:34 +0300

    index page mouse prob

--- resources/views/client/landing.blade.php
+++ resources/views/client/landing.blade.php
@@ -74,7 +74,7 @@
         </div>
     </div>
 </div>
-<div class="section">
+<div class="section mouse-top">
     <div class="container">
         <div class="row">
             <div class="col-12 mb-5 ml-5">
