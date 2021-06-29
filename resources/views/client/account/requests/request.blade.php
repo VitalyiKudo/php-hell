51c7df09079a5a3ad86daceffe0a96dd9b30276c
Zyoga13 <77449355+Zyoga13@users.noreply.github.com>
2021-06-29 18:46:09 +0300

    Delete airplanes type and Max bags

--- resources/views/client/account/requests/request.blade.php
+++ resources/views/client/account/requests/request.blade.php
@@ -231,7 +231,6 @@
                                             <img src="{{ asset('images/max_bags.svg') }}" alt="callender">
                                             <div class="card-details-info">
                                                 <span>28</span>
-                                                <span>Max bags</span>
                                                 <span>Cubic feet</span>
                                             </div>
                                         </li>
@@ -265,9 +264,7 @@
                                         </li>
                                     </ul>
                                 </div>
-                                
-                                <span class="span-airplane">Piper Cheyenne 4, Pilatus PC12, Merlin 3B, King air 90, King air 350, King air 200, TBM 850, Cessna caravan 2.</span>
-                                
+                                                              
                                 <div class="book">
                                     <button type="submit" class="btn rquest-best-price">Request for a best price</button> <a href="{{ route('client.orders.confirm', [$params['searchId'], 'turbo'] ) }}" class="btn book-now">Book now</a>
                                 </div>
@@ -352,7 +349,6 @@
                                             <img src="{{ asset('images/max_bags.svg') }}" alt="callender">
                                             <div class="card-details-info">
                                                 <span>45</span>
-                                                <span>Max bags</span>
                                                 <span>Cubic feet</span>
                                             </div>
                                         </li>
@@ -387,9 +383,7 @@
                                     </ul>
                                 </div>
  
-                                <span class="span-airplane">Phenom 300, Sabreliner 40, Nextant 400, Learjet 45, Hawker 400, Falcon 10/100, Eclipse 500, Citation 5, Cessna CJ2, Beechcraft 400 XP.</span>
-
-                                <div class="book">
+                                 <div class="book">
                                     <button type="submit" class="btn rquest-best-price">Request for a best price</button> <a href="{{ route('client.orders.confirm', [$params['searchId'], 'light'] ) }}" class="btn book-now">Book now</a>
                                 </div>
 
@@ -473,7 +467,6 @@
                                             <img src="{{ asset('images/max_bags.svg') }}" alt="callender">
                                             <div class="card-details-info">
                                                 <span>125</span>
-                                                <span>Max bags</span>
                                                 <span>Cubic feet</span>
                                             </div>
                                         </li>
@@ -507,8 +500,6 @@
                                         </li>
                                     </ul>
                                 </div>
-
-                                <span class="span-airplane">Challenger 300, Citation 10, Falcon 20, Falcon 50, G150 Astra, Gulfstream G200, Hawker 800, Learjet 60, Legacy 450, Sebreliner 75, Westwind 2.</span>
 
                                 <div class="book">
                                     <button type="submit" class="btn rquest-best-price">Request for a best price</button> <a href="{{ route('client.orders.confirm', [$params['searchId'], 'medium'] ) }}" class="btn book-now">Book now</a>
@@ -597,7 +588,6 @@
                                             <img src="{{ asset('images/max_bags.svg') }}" alt="callender">
                                             <div class="card-details-info">
                                                 <span>226</span>
-                                                <span>Max bags</span>
                                                 <span>Cubic feet</span>
                                             </div>
                                         </li>
@@ -631,8 +621,6 @@
                                         </li>
                                     </ul>
                                 </div>
-
-                                <span class="span-airplane">Hawker 4000, Legacy 600, Gulfstream 5, Global 6000, Falcon 900, Falcon 2000, Embraer lineage 1000, Challenger 605</span>
 
                                 <div class="book">
                                     <button type="submit" class="btn rquest-best-price">Request for a best price</button> <a href="{{ route('client.orders.confirm', [$params['searchId'], 'heavy'] ) }}" class="btn book-now">Book now</a>
