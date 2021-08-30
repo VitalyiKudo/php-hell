3811859b58745829f72385799f2abb14e3ea1807
Yuriy Odaryuk <ju.odarjuk@gmail.com>
2021-08-30 12:17:44 +0300

    Fixed square production

--- resources/views/client/account/orders/request_square.blade.php
+++ resources/views/client/account/orders/request_square.blade.php
@@ -5,7 +5,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     
-    <script type="text/javascript" src="{{ $search_type == $upper_case_environment  ? "https://js.squareup.com/v2/paymentform" : "https://js.squareupsandbox.com/v2/paymentform" }}"></script>
+    <script type="text/javascript" src="{{ 'PRODUCTION' == $upper_case_environment  ? "https://js.squareup.com/v2/paymentform" : "https://js.squareupsandbox.com/v2/paymentform" }}"></script>
     <script type="text/javascript">
         window.applicationId = "{{ $applicationId }}";
         window.locationId = "{{ $locationId }}";
