a3c857b8465e62a4c5e99b1985370b77617f29ec
andrii <dron74961@gmail.com>
2021-09-19 20:14:41 +0300

    fix home page

--- resources/views/client/landing.blade.php
+++ resources/views/client/landing.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 {{--    <search-form></search-form>--}}
 
-<div class="section main-search">
+<div class="section main-search fix-content">
     <div class="container">
         <div class="row">
             <div class="offset-md-1">
