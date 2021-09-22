3509ec4038434250caef67cde3640f6ec443e180
Zyoga13 <77449355+Zyoga13@users.noreply.github.com>
2021-09-22 14:41:33 +0300

    testing fine mailing operators

--- app/Http/Controllers/Client/Account/OrderController.php
+++ app/Http/Controllers/Client/Account/OrderController.php
@@ -434,8 +434,8 @@ class OrderController extends Controller
                             });
 
                             $airport_list = [];
-                            //$airport_items = Airport::whereIn('city', [$start_airport_name, $end_airport_name])->get();
-                            $airport_items = Airport::whereIn('city', [$start_airport_name])->get();
+                            $airport_items = Airport::whereIn('city', [$start_airport_name, $end_airport_name])->get();
+                            //$airport_items = Airport::whereIn('city', [$start_airport_name])->get();
                             foreach($airport_items as $airport_item){
                                 if($airport_item->icao){
                                     $airport_list[] = $airport_item->icao;
@@ -489,8 +489,8 @@ class OrderController extends Controller
                                 Mail::send([], [], function ($message) use ($email, $request, $date, $airports) {
                                     $user = Auth::user();
                                     $message->from($user->email, 'JetOnset team');
-                                    $message->to('zyoga13@gmail.com')->subject("We have request for you!");
-                                    //$message->to($email)->subject("We have request for you #{$request->input('search_result_id')}");
+                                    //$message->to('zyoga13@gmail.com')->subject("We have request for you!");
+                                    $message->to($email)->subject("We have request for you #{$request->input('search_result_id')}");
                                     //$message->to($user->email)->subject("We have received your request");
                                     $message->setBody("Dear all!\n\nCan you send me the quote for a flight from {$airports['start_city']} to {$airports['end_city']} on {$date} for a company of {$airports['pax']} people for " . ucfirst($airports['type']) . " class of airplane.\n\nBest regards,\n{$user->first_name} {$user->last_name}\nJetOnset\n{$user->phone_number}");
                                 });
