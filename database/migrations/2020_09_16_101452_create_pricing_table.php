e4451b46270e658ce8e58fb056b3fffd73f0b668
sgitas <serg.gitas@gmail.com>
2021-11-24 18:15:17 +0200

    fix migrate 20200916

--- database/migrations/2020_09_16_101452_create_pricing_table.php
+++ database/migrations/2020_09_16_101452_create_pricing_table.php
@@ -37,6 +37,6 @@ class CreatePricingTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pricing');
+        Schema::dropIfExists('pricings');
     }
 }
