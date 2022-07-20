6c42677f56e884cb2be52ce0415bc3baa55a60f3
sgitas <serg.gitas@gmail.com>
2022-07-20 23:32:54 +0300

    start request

--- app/Jobs/SendEmailOperatorDailyJobs.php
+++ app/Jobs/SendEmailOperatorDailyJobs.php
@@ -59,7 +59,7 @@ class SendEmailOperatorDailyJobs implements ShouldQueue
                     Mail::send([], [$email], function ($message) use ($email) {
                         $message->from('charter@jetonset.com', 'JetOnset team');
                         $message->to($email);
-                        $message->subject("Could you be interested in providing us with the information about your empty legs?");
+                        $message->subject("charters request");
                         $message->setBody(
                             "Dear All,\n\nLet me introduce ourselves as JetOnSet, a young, fast-growing brokerage company.\nWe’re expanding and looking for new reliable partners. Are you interested in cooperation and selling more flights?\nCould you be interested in providing us with the information about your empty legs? We could advertise them on our website under special offers.\nIn addition, please let us know which types of aircrafts you have available and how many, so that we have an idea of the amount of orders we can process with you.\nLooking forward to hearing from you!\n\nKind regards,\nAnastasia\n");
                     });
